f4483f3d893e29943f08d09b1f75232e863e5f03
Anthony Nguyen <antoniousnguyen@gmail.com>
2016-09-23 21:50:39 -0700

    Uses cout instead of printf() and shows how to use a variable with cout.

---
+++ nguyen_hw01_bonus01.cpp
@@ -0,0 +1,19 @@
+// Anthony Nguyen
+// CPSC 120-19
+// HW 01 BONUS 01
+// Bonus: Yes
+// Desc: Outputs my name and age.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Variables
+	int myAge = 25;
+	
+	//Statement that outputs variables.
+	cout << "Hi, I'm Anthony. I am " << myAge << " years old." << endl;
+	
+	return 0;
+}
