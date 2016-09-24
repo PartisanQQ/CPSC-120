78360339deb2b3418a783d2f44edb29cda919e5b
Anthony Nguyen <antoniousnguyen@gmail.com>
2016-09-23 21:49:26 -0700

    Using printf(), includes use of %d to insert variable into statement.

---
+++ nguyen_hw01.cpp
@@ -0,0 +1,18 @@
+// Anthony Nguyen
+// CPSC 120-19
+// HW 01
+// Bonus: Yes
+// Desc: Outputs my name and age.
+
+#include <stdio.h>
+
+int main()
+{
+	//Variables
+	int myAge = 25;
+	
+	//Statement that outputs variables.
+	printf("Hi, I'm Anthony. I am %d years old.\n", myAge);
+	
+	return 0;
+}
