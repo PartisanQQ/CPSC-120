8c97332e3c3510786f695a496a40f85dd539d9b7
Anthony Nguyen <antoniousnguyen@gmail.com>
2016-09-23 21:51:43 -0700

    Shows cout with multiple variables used instead of hardcoding into string.

---
+++ nguyen_hw01_bonus02.cpp
@@ -0,0 +1,21 @@
+// Anthony Nguyen
+// CPSC 120-19
+// HW 01 BONUS 02
+// Bonus: Yes
+// Desc: Outputs my name and age.
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Variables
+	int myAge = 25;
+	string myName = "Anthony";
+	
+	//Statement that outputs variables.
+	cout << "Hi, I'm " << myName << ". I am " << myAge << " years old." << endl;
+	
+	return 0;
+}
